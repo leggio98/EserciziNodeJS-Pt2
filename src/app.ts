f854d433c61eb4fbb1b76bde835a0bf83e54680b
Andrea Barberio <99880853+AndreaBarberio@users.noreply.github.com>
2022-09-14 18:16:23 +0200

    solved test() problems reinstalling supertest, jest ts connections as devDependencies

--- src/app.ts
+++ src/app.ts
@@ -15,4 +15,24 @@ app.get("/planets", (request, response) => {
     );
 });
 
+// MODIFY IT INTO
+
+// //GET /planets  - retrieve all the planets
+// app.get("/planets", (request, response) => {});
+
+// //GET /planets/:id   -retrieve a specific planet
+// app.get("/planet/:id", (request, response) => {});
+
+// //POST /planets   -Create a new planet
+// app.post("/planets", (request, response) => {});
+
+// //PUT /planets/:id   -Replace an existing planet
+// app.put("/planets/:id", (request, response) => {});
+
+// //DELETE /planets/:id    - Delete an existing planet
+// app.delete("/planets/:id", (request, response) =>{});
+
+// //POST /planets/:id/photo - Add a photo for a planet
+// app.post("/planets/:id/photo", (request, response)=>{})
+
 export default app;
