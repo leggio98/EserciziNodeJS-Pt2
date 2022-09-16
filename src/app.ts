6a9c81f26c9ca31cc3b93772b3b111b3dab5f15f
Andrea <andrea.barberio97@gmail.com>
2022-09-16 11:34:38 +0200

    Update: none

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
 import express from "express";
 import "express-async-errors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient(); 
 
-app.get("/planets", (request, response) => {
-    response.json([
-        {
-            name: "Mercury",
-        },
 
-        {
-            name: "Venus",
-        }]
-    );
+app.get("/planets", async (request, response) => {
+   
+    const planets = await prisma.planet.findMany();
+
+    response.json(planets);
 });
 
 export default app;
