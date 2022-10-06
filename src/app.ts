a557c657b4c89fa36c1a95b2e1ecbcb6ef27ede5
Leggio <leggio98@msn.com>
2022-10-06 17:46:33 +0200

    RetrieveAllResources

--- src/app.ts
+++ src/app.ts
@@ -2,32 +2,31 @@ import express from "express";
 import "express-async-errors";
 import prisma from "./lib/prisma/client";
 const app = express();
+app.use(express.json())
 
 app.get("/planets", async (request, response) => {
     const planets = await prisma.planet.findMany();
 
     response.json(planets);
 });
+app.post("/planets", async (request, response) => {
+    const planet = request.body;
+
+    response.status(201).json(planet);
+});
 
 // Request examples
 
 // //GET /planets  - retrieve all the planets
 // app.get("/planets", (request, response) => {});
-
 // //GET /planets/:id   -retrieve a specific planet
 // app.get("/planet/:id", (request, response) => {});
-
 // //POST /planets   -Create a new planet
 // app.post("/planets", (request, response) => {});
-
 // //PUT /planets/:id   -Replace an existing planet
 // app.put("/planets/:id", (request, response) => {});
-
 // //DELETE /planets/:id    - Delete an existing planet
 // app.delete("/planets/:id", (request, response) =>{});
-
 // //POST /planets/:id/photo - Add a photo for a planet
 // app.post("/planets/:id/photo", (request, response)=>{})
-
 export default app;
-
--- src/server.test.ts
+++ src/server.test.ts
@@ -1,14 +1,13 @@
 import supertest from "supertest";
 import app from "./app";
 import { prismaMock } from "./lib/prisma/client.mock";
-
 const request = supertest(app);
-
 test("GET /planets", async () => {
     const planets = [
         {
             id: 1,
             name: "Mercury ",
+            name: "Mercury",
             description: "Solar System First planet",
             diameter: 1234,
             moons: 1545,
@@ -25,15 +24,27 @@ test("GET /planets", async () => {
             updateAt: "2022-09-16T09:50:41.239Z",
         },
     ];
-
     //@ts-ignore
     prismaMock.planet.findMany.mockResolvedValue(planets);
-
     const response = await request
         .get("/planets")
         .expect(200)
         .expect("Content-Type", /application\/json/);
-
     expect(response.body).toEqual(planets);
 });
 
+test("POST /planets", async () => {
+    const planet = {
+        name: "Mercury",
+        diameter: 1234,
+        moons: 1545,
+    };
+
+    const response = await request
+        .post("/planets")
+        .send(planet)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toEqual(planet);
+});
